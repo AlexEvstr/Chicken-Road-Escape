53cc81018216428f700ae1920d149ea78b43326d
Alexander Evstratov <alexdrugev@me.com>
2025-02-20 11:51:12 +0300

    Start ermaking menu.

--- Assets/GameFolders/=Scripts/MenuOptions.cs
+++ Assets/GameFolders/=Scripts/MenuOptions.cs
@@ -3,19 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuOptions : MonoBehaviour
 {
-    [SerializeField] private GameObject _onSound;
-    [SerializeField] private GameObject _offSound;
-    [SerializeField] private GameObject _onMusic;
-    [SerializeField] private GameObject _offMusic;
-    [SerializeField] private GameObject _onVibro;
-    [SerializeField] private GameObject _offVibro;
+    [SerializeField] private GameObject _soundON;
+    [SerializeField] private GameObject _soundOFF;
+    [SerializeField] private GameObject _musicON;
+    [SerializeField] private GameObject _musicOFF;
+    [SerializeField] private GameObject _vibroON;
+    [SerializeField] private GameObject _vibroOFF;
 
-    [SerializeField] private AudioSource _soundController;
-    [SerializeField] private AudioSource _musicController;
+    [SerializeField] private AudioSource _soundManager;
+    [SerializeField] private AudioSource _musicManager;
 
-    [SerializeField] private AudioClip _clickSound;
-    [SerializeField] private AudioClip _buySound;
-    [SerializeField] private AudioClip _failSound;
+    [SerializeField] private AudioClip _click;
+    [SerializeField] private AudioClip _buy;
+    [SerializeField] private AudioClip _fail;
 
     private float _vibration;
 
@@ -23,95 +23,106 @@ public class MenuOptions : MonoBehaviour
     {
         Vibration.Init();
 
-        _soundController.volume = PlayerPrefs.GetFloat("SoundVolume", 1);
-        if (_soundController.volume == 1)
+        _soundManager.volume = PlayerPrefs.GetFloat("SoundVolume", 1);
+        if (_soundManager.volume != 1)
         {
-            _offSound.SetActive(true);
-            _onSound.SetActive(false);
+            _soundOFF.SetActive(true);
+            _soundON.SetActive(false);
         }
         else
         {
-            _onSound.SetActive(true);
-            _offSound.SetActive(false);
+            _soundON.SetActive(true);
+            _soundOFF.SetActive(false);
         }
 
-        _musicController.volume = PlayerPrefs.GetFloat("MusicVolume", 1);
-        if (_musicController.volume == 1)
+        _musicManager.volume = PlayerPrefs.GetFloat("MusicVolume", 1);
+        if (_musicManager.volume != 1)
         {
-            _offMusic.SetActive(true);
-            _onMusic.SetActive(false);
+            _musicOFF.SetActive(true);
+            _musicON.SetActive(false);
         }
         else
         {
-            _onMusic.SetActive(true);
-            _offMusic.SetActive(false);
+            _musicON.SetActive(true);
+            _musicOFF.SetActive(false);
         }
 
         _vibration = PlayerPrefs.GetFloat("VibroStatus", 1);
+
+        if (_vibration != 1)
+        {
+            _vibroOFF.SetActive(true);
+            _vibroON.SetActive(false);
+        }
+        else
+        {
+            _vibroON.SetActive(true);
+            _vibroOFF.SetActive(false);
+        }
     }
 
     public void DisableSound()
     {
-        _onSound.SetActive(true);
-        _offSound.SetActive(false);
-        _soundController.volume = 0;
+        _soundON.SetActive(false);
+        _soundOFF.SetActive(true);
+        _soundManager.volume = 0;
         PlayerPrefs.SetFloat("SoundVolume", 0);
     }
 
     public void EnableSound()
     {
-        _offSound.SetActive(true);
-        _onSound.SetActive(false);
-        _soundController.volume = 1;
+        _soundOFF.SetActive(false);
+        _soundON.SetActive(true);
+        _soundManager.volume = 1;
         PlayerPrefs.SetFloat("SoundVolume", 1);
     }
 
     public void DisableMusic()
     {
-        _onMusic.SetActive(true);
-        _offMusic.SetActive(false);
-        _musicController.volume = 0;
+        _musicON.SetActive(false);
+        _musicOFF.SetActive(true);
+        _musicManager.volume = 0;
         PlayerPrefs.SetFloat("MusicVolume", 0);
     }
 
     public void EnableMusic()
     {
-        _offMusic.SetActive(true);
-        _onMusic.SetActive(false);
-        _musicController.volume = 1;
+        _musicOFF.SetActive(false);
+        _musicON.SetActive(true);
+        _musicManager.volume = 1;
         PlayerPrefs.SetFloat("MusicVolume", 1);
     }
 
     public void DisableVibro()
     {
-        _onVibro.SetActive(true);
-        _offVibro.SetActive(false);
+        _vibroON.SetActive(false);
+        _vibroOFF.SetActive(true);
         PlayerPrefs.SetFloat("VibroStatus", 0);
     }
 
     public void EnableVibro()
     {
-        _offVibro.SetActive(true);
-        _onVibro.SetActive(false);
+        _vibroOFF.SetActive(false);
+        _vibroON.SetActive(true);
         PlayerPrefs.SetFloat("VibroStatus", 1);
     }
 
     public void PlayClickSound()
     {
         if (_vibration == 1) Vibration.VibratePop();
-        _soundController.PlayOneShot(_clickSound);
+        _soundManager.PlayOneShot(_click);
     }
 
     public void PlayBuySound()
     {
         if (_vibration == 1) Vibration.VibratePeek();
-        _soundController.PlayOneShot(_buySound);
+        _soundManager.PlayOneShot(_buy);
     }
 
     public void PlayFailSound()
     {
         if (_vibration == 1) Vibration.VibrateNope();
-        _soundController.PlayOneShot(_failSound);
+        _soundManager.PlayOneShot(_fail);
     }
 
     public void ClearAllData()
